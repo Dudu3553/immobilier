fd136547daa99d0f5d28b9610887a7a06117629a
Kevin DUMAST <kevin.dumast@orange.fr>
2022-01-24 13:24:42 +0100

    add selection commune

---
+++ static/js/data.js
@@ -0,0 +1,12 @@
+function selectionnerDepartement() {
+	// L'utilisateur a cliqué sur la liste déroulante des départements
+	var e = document.getElementById("departements");
+	var sonCode = e.options[e.selectedIndex].value;
+};
+
+
+function selectionnerCommune() {
+	// L'utilisateur a cliqué sur la liste déroulante des communes
+	var e = document.getElementById("communes");
+	var sonCode = e.options[e.selectedIndex].value;
+}
